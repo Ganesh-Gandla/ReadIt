da5fa1f79a00c993644b4cec43f4b2f5aff2c7bd
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-18 17:21:05 +0530

    Added feature to display error massage in error page

--- ReadIt/src/components/Error404.jsx
+++ ReadIt/src/components/Error404.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 import "../App.css";
 
 function Error404() {
+    const error = useRouteError();
+    console.error(error);
+
     return (
         <div className="error-page">
             <h1 className="error-code">404</h1>
             <h2 className="error-title">Page Not Found</h2>
             <p className="error-text">
-                Oops! The page you're looking for doesn't exist or has been moved.
+                {error.data || "The page you are looking for does not exist."}
             </p>
 
             <Link to="/" className="error-btn">Go Back Home</Link>
